24693a394b6841cce2ccd8645d1221d68890993e
nklofy <nklofy@hotmail.com>
2017-03-03 00:05:09 +0800

    update

--- YFVMprj/src/Mem/MemHeap.h
+++ YFVMprj/src/Mem/MemHeap.h
@@ -12,6 +12,25 @@ class MemHeap {
 public:
 	MemHeap();
 	virtual ~MemHeap();
+	int init();
+	long mallocObj(long);
+	long mallocStr(string&);
+	long mallocArray(long,long);
+
+	int freeObj(long);
+	int freeArray(long);
+
+private:
+	int GC();
+	void* block;
+	long block_size=0XFFFF;
+	long block_count=7;
+	char* block_info;
+	long* stack_stab;
+	long** block_stab;
+	int resetBlockSize(long);
+	int resetBlockCount(long);
+
 };
 
 #endif /* SRC_MEM_MEMHEAP_H_ */
